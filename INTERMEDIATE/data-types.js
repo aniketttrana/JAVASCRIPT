2f61e156a5979ba3cad34c1f951638cf298fa266
anikettrana <anikettrana@gmail.com>
2023-09-28 14:19:20 +0530

    data-types

---
+++ INTERMEDIATE/data-types.js
@@ -0,0 +1,28 @@
+/*
+Basic Data TYPES - 2 TYPES
+
+Primitive 
+Non Primitive
+
+1.Primitive Data types: 7 Types
+*/
+
+var name = "Aniket"; // 1. String
+var num = 200; // 2.Number
+var twophonenumber = 94884856777785858585n; //3.Boolean
+var truth = false; // 4. Boolean
+var dob; // 5.undefined
+var date = null; //6.null
+var place = Symbol("Himachal"); //7.Symbol
+
+// 2. Non-Primitive Data types -  n types (will keep increasing)
+
+var student = {
+  //Object
+  firstName: "aniket",
+  lastName: null,
+  class: 19,
+};
+
+var gender = ["male", "female", "trans", "others"]; //2.Arrays
+var mapp = new Set(["a", "b", "c"]); //3. Sets
